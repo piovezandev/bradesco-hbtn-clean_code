7cce7e6958c07ab8ee1e3f638a26f7c29bb1d075
Alan Piovezan <alan.log.piovezan@gmail.com>
2025-10-16 21:07:24 -0300

    clean_code - functions

---
+++ 0x00/function/Functions.java
@@ -0,0 +1,45 @@
+package cleancode_nomenclatura;
+
+public class Functions {
+    // Faça somente 1 coisa
+    //B
+    public void saveIncome(Income income){
+        inRepo.save(income);
+    }
+    public void saveExpense(Expense expense){
+        outRepo.save(expense);
+    }
+
+    //Somente 1 nível de abstração
+    //A
+    @PostMapping("/user")
+    public User saveUser(User user){
+        return userService.save(user);
+    } 
+
+    // Arguments
+    //A
+    public void saveUser(User user){};
+  
+    //  No side effects
+    //A
+    public User saveUser(User user){
+      if(user.isAdmin){
+      user.setRole(user);
+    }
+      return userRepository.save(user);
+    }
+
+    // Evite duplicados, extraindo para novos métodos
+    //B
+    public Environment getEnvironment(){
+        return environmentService.getEnvironment(getVersion());
+    }
+    public Release getRelease(){
+        return releaseService.getRelease(getVersion());
+    }
+    private String getVersion(){ 
+        return versionService.getVersion();
+    }
+
+}
